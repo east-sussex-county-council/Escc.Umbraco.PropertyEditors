b170445ae469b2e2fbd29fd56348bc2236d71c88
richardm <richard.mason@eastsussex.gov.uk>
2014-06-30 12:32:37 +0100

    Refactor example code for validation and apply red border when invalid

--- FilteredRichTextPropertyEditor/controller.js
+++ FilteredRichTextPropertyEditor/controller.js
@@ -1,4 +1,4 @@
-angular.module("umbraco").controller("Escc.Umbraco.PropertyEditors.FilteredRichTextPropertyEditor.Controller", function ($scope, $controller) {
+angular.module("umbraco").controller("Escc.Umbraco.PropertyEditors.FilteredRichTextPropertyEditor.Controller", function ($scope, $controller, assetsService) {
     // Inherit the behaviour of the standard rich text editor
     $controller("Umbraco.PropertyEditors.RTEController", { $scope: $scope });
 
@@ -6,33 +6,54 @@ angular.module("umbraco").controller("Escc.Umbraco.PropertyEditors.FilteredRichT
     // taking care because "0" means false but is a truthy value when tested.
     $scope.model.hideLabel = ($scope.model.config.hideLabel === "1");
 
+    // Load custom CSS for this editor
+    assetsService.loadCss("/App_Plugins/Escc.Umbraco.PropertyEditors.FilteredRichTextPropertyEditor/styles.css");
+
+    setupValidation();
+    setupFilters();
+
     // Validation using the $parsers pipeline and a directive wasn't working with tinyMCE, but can be done from here.
     // The inherited controller updates $scope.model.value from tinyMCE, so watch that change to validate. 
     // Remove the watch when done to avoid a memory leak.
-    var stopWatching = $scope.$watch('model.value', function () {
-        var valid = $scope.model.value.indexOf("example") == -1;
-        $scope.propertyForm.validateThis.$setValidity('exampleValidator', valid);
+    function setupValidation() {
+        var validators = createValidators();
 
-    });
+        var stopWatching = $scope.$watch('model.value', function () {
+            angular.forEach(validators, function (validator) {
+                var valid = validator.validate($scope.model.value);
+                $scope.propertyForm.validateThis.$setValidity(validator.id, valid);
+            });
+        });
 
-    $scope.$on('$destroy', function () {
-        stopWatching();
-    });
+        $scope.$on('$destroy', function () {
+            stopWatching();
+        });
+    }
+
+    // Create the validators which will be checked before the field can be saved.
+    function createValidators() {
+        return [
+            { id: 'exampleValidator', validate: function(value) { return (!value) || value.indexOf("example") == -1; } },
+            { id: 'testValidator', validate: function (value) { return (!value) || value.indexOf("test") == -1; }}
+        ];
+    }
 
     // Apply filters to HTML as it is being saved. Need to get and set the content from the tinyMCE
     // editor rather than $scope.model.value, because the inherited controller above goes on to set 
     // $scope.model.value from tinyMCE, so if we don't change that our work will be overwritten.
-    $scope.$on("formSubmitting", function(ev, args) {
-       var instance = tinymce.get($scope.model.alias + "_rte");
-        var html = instance.getContent();
-        var filters = createFilters();
+    function setupFilters() {
+        $scope.$on("formSubmitting", function (ev, args) {
+            var instance = tinymce.get($scope.model.alias + "_rte");
+            var html = instance.getContent();
+            var filters = createFilters();
 
-        angular.forEach(filters, function(filter) {
-            html = filter(html);
+            angular.forEach(filters, function(filter) {
+                html = filter(html);
+            });
+
+            instance.setContent(html);
         });
- 
-        instance.setContent(html);
-    });
+    }
 
     // Create the filters which will be applied to HTML as it is saved. Each one should be a 
     // function which accepts one string argument and returns a string.
