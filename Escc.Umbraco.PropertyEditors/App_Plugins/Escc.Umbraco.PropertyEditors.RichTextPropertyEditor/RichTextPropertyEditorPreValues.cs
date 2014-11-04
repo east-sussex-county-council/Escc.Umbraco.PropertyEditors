b0f9574ff283b1f161faea36a890aecdf47be4d2
richardm <richard.mason@eastsussex.gov.uk>
2014-11-04 16:21:43 +0000

    Add standard strings used to customise the rich text editor

--- Escc.Umbraco.PropertyEditors/App_Plugins/Escc.Umbraco.PropertyEditors.RichTextPropertyEditor/RichTextPropertyEditorPreValues.cs
+++ Escc.Umbraco.PropertyEditors/App_Plugins/Escc.Umbraco.PropertyEditors.RichTextPropertyEditor/RichTextPropertyEditorPreValues.cs
@@ -1,33 +1,53 @@
 ﻿
+using System.Runtime.Serialization;
+
 namespace Escc.Umbraco.PropertyEditors.App_Plugins.Escc.Umbraco.PropertyEditors.RichTextPropertyEditor
 {
     /// <summary>
     /// Prevalues for an Umbraco data type using Escc.Umbraco.PropertyEditors.RichTextPropertyEditor, which can be serialised to the correct JSON format
     /// </summary>
+    [DataContract]
     public class RichTextPropertyEditorPreValues
     {
+        [DataMember]
         public string[] toolbar { get; set; }
+
+        [DataMember]
         public string[] stylesheets { get; set; }
+
+        [DataMember]
         public Dimensions dimensions { get; set; }
+
+        [DataMember]
         public Validator[] validators { get; set; }
+
+        [DataMember]
         public string[] formatters { get; set; }
     }
 
     /// <summary>
     /// Dimensions for the Escc.Umbraco.PropertyEditors.RichTextPropertyEditor which can be serialised to JSON
     /// </summary>
+    [DataContract]
     public class Dimensions
     {
+        [DataMember]
         public int height { get; set; }
+
+        [DataMember]
         public int width { get; set; }
     }
 
     /// <summary>
     /// Validators for the Escc.Umbraco.PropertyEditors.RichTextPropertyEditor which can be serialised to JSON
     /// </summary>
+    [DataContract]
     public class Validator
     {
+        [DataMember]
         public string name { get; set; }
+
+        [DataMember]
         public int max { get; set; }
     }
 }
